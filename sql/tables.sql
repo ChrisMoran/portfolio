d5add4fdd1aa48a5c2aef227dd94549a9be97a4b
ChrisMoran <christophermoran2012@u.northwestern.edu>
2012-11-10 13:42:20 -0600

    Fixed a few errors when importing tables

--- sql/tables.sql
+++ sql/tables.sql
@@ -7,18 +7,10 @@ create sequence portfolio_id_seq start with 1 increment by 1;
 
 CREATE TABLE Portfolios (
        id number NOT NULL PRIMARY KEY,
-       user_name VARCHAR(100) NOT NULL FOREIGN KEY REFERENCES Users(name),
+       user_name VARCHAR(100) NOT NULL REFERENCES Users(name),
        assets number not null,     
        portfolio_name VARCHAR(100) -- user supplied name for porfolio, optional
 );
-
--- trigger for auto increment functionality for portfolio id
-create or replace trigger portfolio_insert
-before insert on Portfolios
-for each row
-begin
-	select portfolio_id_seq.nextval into :new.id from dual;
-end;
 
 CREATE TABLE AllStockSymbols (
        symbol char(16) not null primary key,
@@ -28,34 +20,41 @@ CREATE TABLE AllStockSymbols (
 );
 
 -- need writable table for new stocks we might add, nice to have all in one table
-INSERT INTO AllStockSymbols VALUES (SELECT * from CS339.StocksSymbols);
+INSERT INTO AllStockSymbols (symbol, count, first, last) (SELECT * from CS339.StocksSymbols);
 
-
-CREATE TABLE AllStocksDaily (
-       symbol char(16) not null foreign key references AllStockSymbols(symbol),
+-- table for new stacks, CS339.StocksDaily too big for copy
+CREATE TABLE NewStocksDaily (
+       symbol char(16) not null references AllStockSymbols(symbol),
        timestamp number not null,
        open number not null,
        high number not null,
        low number not null,
        close number not null,
        volume number not null,
-       constraint pk primary key (symbol, timestamp)
+       constraint pk_asd primary key (symbol, timestamp)
 );
--- writable table for stock values, again, nice to have in one table
-INSERT INTO AllStocksDaily values (SELECT * FROM CS339.StocksDaily);
 
 -- current holdings, select * from holdings where sell_date is null and sell_price is null
 -- 
 CREATE TABLE Holdings (
-       portfolio number NOT NULL FOREIGN KEY REFERENCES Portfolio(id),
-       symbol char(16) NOT NULL FOREIGN KEY REFERENCES AllStockSymbols(symbol),
+       portfolio number NOT NULL REFERENCES Portfolios(id),
+       symbol char(16) NOT NULL REFERENCES AllStockSymbols(symbol),
        shares number NOT NULL, -- number of shares of stock
        purchase_date number NOT NULL, --
        purchase_price number NOT NULL,
        sell_date number default NULL, -- if null haven't sold
        sell_price number default NULL,
-       constraint pk primary key (portfolio, symbol, purchase_date)
+       constraint pk_holdings primary key (portfolio, symbol, purchase_date)
 );
+
+-- trigger for auto increment functionality for portfolio id
+create or replace trigger portfolio_insert
+before insert on Portfolios
+for each row
+begin
+	select portfolio_id_seq.nextval into :new.id from dual;
+end;
+/
 
 -- also need table for caching portfolio stats like coefficient of variation and Beta
 -- not sure what that should look like yet, so no worries
