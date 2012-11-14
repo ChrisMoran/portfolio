c4f131e929ea4e9747aea7ed5a20430b9f23430f
ChrisMoran <christophermoran2012@u.northwestern.edu>
2012-11-14 16:59:25 -0600

    Added buying/selling stocks

--- quote.pl
+++ quote.pl
@@ -4,63 +4,52 @@ use Data::Dumper;
 use CGI qw(:standard);
 use Finance::Quote;
 
-BEGIN {
-  $ENV{PORTF_DBMS}="oracle";
-  $ENV{PORTF_DB}="pzu918";
-  $ENV{PORTF_DBUSER}="pzu918";
-  $ENV{PORTF_DBPASS}="z00wgeGKy";
-
-  unless ($ENV{BEGIN_BLOCK}) {
-    use Cwd;
-    $ENV{ORACLE_BASE}="/raid/oracle11g/app/oracle/product/11.2.0.1.0";
-    $ENV{ORACLE_HOME}=$ENV{ORACLE_BASE}."/db_1";
-    $ENV{ORACLE_SID}="CS339";
-    $ENV{LD_LIBRARY_PATH}=$ENV{ORACLE_HOME}."/lib";
-    $ENV{BEGIN_BLOCK} = 1;
-    exec 'env',cwd().'/'.$0,@ARGV;
-  }
-};
-
+use portfolio_util;
 use stock_data_access;
+
 #declare subroutine trim
 sub trim($);
 
 #need to change later
-$portfolio = 1;
+my $portfolio = param('id');
+my $userCookie = cookie('portSession');
+if(defined($userCookie) && defined($portfolio)) {
+    my ($userLogin,$password) = split(/\//, $userCookie);
+    if(ValidUser($userLogin, $password)) {
+	my @info=("date","time","high","low","close","open","volume");
+	my @symbols = ExecStockSQL('COL',"SELECT symbol FROM Holdings WHERE portfolio=?", $portfolio);
+	my $con=Finance::Quote->new();
 
-@info=("date","time","high","low","close","open","volume");
+	$con->timeout(60);
 
-@symbols = ExecStockSQL('COL',"SELECT symbol FROM PZU918.Holdings WHERE portfolio=$portfolio");
-$con=Finance::Quote->new();
-
-$con->timeout(60);
-
-%quotes = $con->fetch("usa",@symbols);
-print "Content-type: text/html\n\n";
-print "<td><a href=\"newtrade.pl?act=newtrade&portfolio=$portfolio\">BUY A NEW STOCK</a></td><br>";
-print "<table>";
-print "<tr><th>Symbol</th><th>Date</th><th>Time</th><th>High</th><th>Low</th><th>Close</th><th>Open</th><th>Volume</th><th>Shares</th><th>Action</th></tr>";
-foreach $symbol (@symbols) {
-    $symbol = trim($symbol);
-    print "<tr>";
-    print "<td>",$symbol,"</td>";
-    if (!defined($quotes{$symbol,"success"})) { 
-	print "<td colspan=\"7\">No Data</td>";
-    } else {
-
-	foreach $key (@info) {
-	    if (defined($quotes{$symbol,$key})) {
-		print "<td>",$quotes{$symbol,$key},"</td>";
+	my %quotes = $con->fetch("usa",@symbols);
+	print "Content-type: text/html\n\n";
+	print "<td><a href=\"newtrade.pl?act=newtrade&id=$portfolio\">BUY A NEW STOCK</a></td><br>";
+	print "<table>";
+	print "<tr><th>Symbol</th><th>Date</th><th>Time</th><th>High</th><th>Low</th><th>Close</th><th>Open</th><th>Volume</th><th>Shares</th><th>Action</th></tr>";
+	foreach $symbol (@symbols) {
+	    $symbol = trim($symbol);
+	    print "<tr>";
+	    print "<td>",$symbol,"</td>";
+	    if (!defined($quotes{$symbol,"success"})) { 
+		print "<td colspan=\"7\">No Data</td>";
+	    } else {
+		
+		foreach $key (@info) {
+		    if (defined($quotes{$symbol,$key})) {
+			print "<td>",$quotes{$symbol,$key},"</td>";
+		    }
+		}
+		@shares = ExecStockSQL('COL',"SELECT shares FROM Holdings WHERE portfolio = ? AND symbol = rpad(?, 16)", $portfolio, $symbol);
+		print "<td>",$shares[0],"</td>";
+		print "<td><a href=\"newtrade.pl?act=newtrade&id=$portfolio&stock=$symbol\">New Trade</a></td>";
 	    }
+	    print "</tr>";
 	}
-	@shares = ExecStockSQL('COL',"SELECT shares FROM PZU918.Holdings WHERE portfolio = $portfolio AND symbol = \'$symbol\'");
-	print "<td>",$shares[0],"</td>";
-	print "<td><a href=\"newtrade.pl?act=newtrade&portfolio=$portfolio&stock=$symbol\">New Trade</a></td>";
+	print "</table><br/>";
+	print "<a href=\"userHome.pl\">Return to home</a>";
     }
-    print "</tr>";
 }
-print "</table>";
-
 
 #trim($)
 #to remove space at the beginning and the end;
